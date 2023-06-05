203fa075a54d01a46f433442944b75e6816abfe1
Pearl Baghel <89574835+Pearl-Baghel@users.noreply.github.com>
2023-06-05 20:52:23 +0530

    first commit

---
+++ Tutorials/src/lec1.java
@@ -0,0 +1,7 @@
+class lec1 {
+    // This is a standard format of writing a code in java we always write code in form of class and function 
+    public static void main(String args[]){
+
+    }
+    
+}
